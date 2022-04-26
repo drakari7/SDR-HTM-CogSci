f05c32b0bbfbbe3a483846e496984a88c232dd79
drakari7 <shreyash.ravi@gmail.com>
2022-04-26 16:51:06 +0530

    Update: Modified sdr class file and generated graphs justifying probability approximation
    Certain functions which are properties of a given SDR instance have been
    incorporated as methods in the SDR class. Other functions which depend
    on parameters like (n, w) have their own methods.
    
    property_graph.py generates graphs justifying the false probability
    approximation, as the value is > 90% of original value for noise < 50%.

---
+++ property_graph.py
@@ -0,0 +1,39 @@
+from math import comb
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from sdr import SDR
+import sdr as sdr_tools
+
+def generate_random_sdr(n, w):
+    rand_positions = random.sample(range(n), w)
+    res = np.zeros(n)
+    res[rand_positions] = 1
+    return res
+
+
+def main():
+    n, w = 1024, 40
+    a = SDR(generate_random_sdr(n, w))
+
+    thetas = list(range(0, w+1, 1))
+    ratios = []
+
+    for theta in thetas:
+        v1 = sdr_tools.exact_prob_fp(a, w, theta)
+        v2 = sdr_tools.approx_prob_fp(a, w, theta)
+
+        ratio = v2/v1
+        ratios.append(ratio)
+        
+    noise = [1 - theta/w for theta in thetas[::-1]]
+    plt.plot(noise, ratios[::-1])
+    plt.xlabel(f"Percentage noise for n = {n}, w = {w}")
+    plt.ylabel(f"approximate probability / exact probability")
+    plt.title(f"Variation of false probability approximation")
+    plt.grid()
+    plt.savefig("./graphs/fp_approximation.jpg", dpi=250)
+
+if __name__ == "__main__":
+    main()
--- sdr.py
+++ sdr.py
@@ -2,44 +2,65 @@ import numpy as np
 from math import comb
 
 class SDR:
-    def __init__(self, sdr:np.array):
-        self.vec = sdr
-        self.n = len(sdr)
-        self.w = np.sum(sdr)
-        self.s = self.w/self.n
-    
-    def __mul__(self, other):
-        return np.sum(np.logical_and(self.vec,other.vec))
-    
-def match(sdr1, sdr2, theta):
-    return sdr1 * sdr2 >= theta
+    vec:    np.ndarray          # Vector of n neurons
+    n:      int                 # Dimension of vector
+    w:      int                 # No of ON bits
+    s:      float               # Sparsity measure
 
-def uniq_sdr(n, w):
+    def __init__(self, sdr):
+        self.vec = np.asarray(sdr)
+        self.n = len(self.vec)
+        self.w = np.count_nonzero(self.vec)
+        self.s = self.w/self.n
+
+    ## Methods defined here are direct properties of SDRs
+    # Returns size of overlap with another SDR
+    def overlap(self, other) -> int:
+        return np.dot(self.vec, other.vec)
+
+    # Is there a match with threshhold theta?
+    def match(self, other, theta) -> bool:
+        return self.overlap(other) >= theta
+
+    # Size of overlap set for some value b and w_y
+    def overlap_set(self, w_y, b) -> int:
+        return comb(self.w, b) * comb(self.n - self.w, w_y - b)
+
+## General functions which are not properties of SDRs
+
+# Number of unique SDRs for given n and w
+def uniq_sdr(n, w) -> int:
     return comb(n, w)
 
-def prob_identical(n, w):
+# Prob that 2 random SDRs are identical
+def prob_identical(n, w) -> float:
     return 1/comb(n, w)
 
-def num_overlap(sdr, w, b):
-    return comb(sdr.w, b) * comb(sdr.n - sdr.w, w - b)
+# Exact probability of false positive match of sdr with parameters (w, theta)
+def exact_prob_fp(sdr:SDR, w, theta) -> float:
+    sum = 0
+    for b in range(theta, w+1):
+        sum += sdr.overlap_set(w, b)
 
-def prob_fp(sdr, w, theta):
-    return np.sum([num_overlap(sdr, w, i) for i in range(theta, w+1, 1)]) / uniq_sdr(sdr.n, w)
+    denom = uniq_sdr(sdr.n, w)
+    return sum/denom
 
-def approx_prob_fp(sdr, w, theta):
-    return num_overlap(sdr, w, theta) / uniq_sdr(sdr.n, w)
+# Approximation of above function
+def approx_prob_fp(sdr:SDR, w, theta) -> float:
+    return sdr.overlap_set(w, theta) / uniq_sdr(sdr.n, w)
 
-#For sub sampling pass :: w_y as w
+# def check_match(X, theta):
+#     for x in X:
+#         for y in X:
+#             if x!=y and match(x, y, theta):
+#                 return False
+#     return True
 
-def check_match(X, theta):
-    for x in X:
-        for y in X:
-            if x!=y and match(x, y, theta):
-                return False
-    return True
+def main():
+    # For testing without affecting other modules that import this
+    pass
 
-a = np.array([1,1,0])
-b = np.array([1,0,1])
-print(SDR(a)*SDR(b)) 
 
+if __name__ == "__main__":
+    main()
 
